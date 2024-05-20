e1171917b9dda2968c8d80e5555428f1254e9a07
Kunhalmi Attila <79263624+Atoo97@users.noreply.github.com>
2024-05-20 09:54:33 +0200

    Add files via upload

---
+++ C# projects/MAUI/SnakeGame/SnakeGame/SnakeGame/AppShell.xaml.cs
@@ -0,0 +1,144 @@
+﻿using SnakeLib.Model;
+using SnakeLib.Persistence;
+using SnakeGame.View;
+using SnakeGame.ViewModel;
+using Microsoft.Maui.Dispatching;
+using System.Threading;
+
+namespace SnakeGame
+{
+    public partial class AppShell : Shell
+    {
+        #region Fields
+        private bool notFirstSatrt = false; // első indtást jelzi
+
+        private ISnakeDataAccess _snakeDataAccess;
+        private readonly SnakeModel _model;
+        private readonly SnakeViewModel _viewModel;
+        private IDispatcherTimer _timer = null!;
+        #endregion
+
+        public AppShell(ISnakeDataAccess snakeDataAccess,SnakeModel model, SnakeViewModel viewModel)
+        {
+            InitializeComponent();
+
+            // játék összeállítása
+            _snakeDataAccess = snakeDataAccess;
+
+            _model = model;
+            _model.GameOver += new EventHandler<SnakeEventArgs>(Model_GameOver);
+
+            _viewModel = viewModel;
+            _viewModel.LoadGame += SnakeViewModel_LoadGame;
+            _viewModel.StartStopGame += new EventHandler(SnakeViewModel_StartStopGame);
+            _viewModel.RestartGame += new EventHandler(ViewModel_RestartGame);
+
+            //időzítő létrehoz
+            _timer = Dispatcher.CreateTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(500);
+            _timer.Tick += new EventHandler(Timer_Tick);
+
+
+            //kezdeti large pályabetöltése
+            _viewModel.SizeChangeCommand.Execute("Large");
+        }
+
+
+        #region ViewModel event handlers
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            _model.AdvanceTime();
+        }
+
+        /// <summary>
+        /// Játék betöltésének/ pályaméretváltásának eseménykezelője.
+        /// </summary>
+        private async void SnakeViewModel_LoadGame(object sender, EventArgs e)
+        {
+            if (notFirstSatrt) //első indításkor NEM lefutó elágazás
+            {
+                bool answer = await DisplayAlert("Figyelem!",
+                                                "Biztos pályát váltasz?", "Yes", "No");
+                //_model.RestartGame();
+            }
+
+            notFirstSatrt = true;
+
+            _timer.Stop();
+            await  _model.LoadGameAsync();
+        }
+
+        /// <summary>
+        /// Játék indításának eseménykezelője.
+        /// </summary>
+        private void SnakeViewModel_StartStopGame(object sender, System.EventArgs e)
+        {
+            if (_timer.IsRunning)
+            {
+                if (_model.IsGamePaused == false)
+                {
+                    _model.SetGamePaused(true);
+                }
+                else
+                {
+                    _model.SetGamePaused(false);
+                }
+            }
+            else
+            {
+                _model.NewGame();
+                _timer.Start();
+            }
+            
+        }
+
+        /// <summary>
+        /// Játék újraindításának eseménykezelője.
+        /// </summary>
+        private async void ViewModel_RestartGame(object sender, System.EventArgs e)
+        {
+            bool answer = await DisplayAlert("Figyelem!",
+                                               "Biztos újra kezded a játékot?", "Yes", "No");
+            if (answer)
+            {
+                await _model.LoadGameAsync();
+                _model.RestartGame();
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Játék végének eseménykezelője.
+        /// </summary>
+        private async void Model_GameOver(Object sender, SnakeEventArgs e)
+        {
+            _timer.Stop(); //leáll az idő
+
+            if (e.IsOver) //ha a kigyó akadályba ütközött
+            {
+                bool answer = await DisplayAlert("Vége a játéknak! Akadályba ütköztél!" + Environment.NewLine +
+                                                 "Összesen " + e.ScoresCount + " tojást sikerült megenned.",
+                                                "Új játékot kezdesz?", "Yes", "No");
+                if (answer)
+                {
+                    await _model.LoadGameAsync();
+                    _model.NewGame();
+                }
+            }
+            else //ha a kigyó saját magába harapottgo
+            {
+                bool answer = await DisplayAlert("Vége a játéknak! A kigyó öngyilkos lett!" + Environment.NewLine +
+                                                    "Összesen " + e.ScoresCount + " tojást sikerült megenned.",
+                                                  "Új játékot kezdesz?", "Yes", "No");
+                if (answer)
+                {
+                    await _model.LoadGameAsync();
+                    _model.NewGame();
+                }
+            }
+
+        }
+
+    }
+}
---
+++ C# projects/MAUI/SnakeGame/SnakeGame/SnakeGame/ViewModel/SnakeFileDataAccess.cs
@@ -0,0 +1,120 @@
+﻿using SnakeGame.Model;
+using SnakeLib.Model;
+using SnakeLib.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame.ViewModel
+{
+    /// <summary>
+    /// Snake fájlkezelő típusa.
+    /// </summary>
+    public class SnakeFileDataAccess : ISnakeDataAccess
+    {
+        /// <summary>
+        /// Könyvtár.
+        /// </summary>
+        private string _directory = string.Empty;
+        private string _line = string.Empty;
+
+        public SnakeFileDataAccess(string saveDirectory)
+        {
+            _directory = saveDirectory;
+
+        }
+
+
+        /// <summary>
+        /// Fájl betöltése.
+        /// </summary>
+        /// <param name="_directory">Elérési útvonal.</param>
+        /// <returns>A fájlból beolvasott játéktábla.</returns>
+        public async Task<SnakeTable> LoadAsync(GameTableSize tablesize)
+        {
+
+            if (string.IsNullOrEmpty(_directory))
+                throw new SnakeDataException("Hibás elérési útvonal!");
+
+            int? length = null;
+
+            try
+            {
+                //StreamReader reader = new StreamReader(_directory!)
+                using var stream = await FileSystem.OpenAppPackageFileAsync(_directory);
+                using (StreamReader reader = new StreamReader(stream)) // fájl megnyitása
+                {
+                    switch (tablesize)
+                    {
+                        case GameTableSize.Small:
+                            length = 0;
+                            break;
+                        case GameTableSize.Medium:
+                            length = 1;
+                            break;
+                        case GameTableSize.Large:
+                            length = 2;
+                            break;
+                    }
+
+
+                    //Beolvasás megváltoztat table-re!
+                    string[] datas = null!;
+                    for (int i = -1; i < length; i++)
+                    {
+                        string line = await reader.ReadLineAsync() ?? string.Empty;
+                        datas = line.Split(' '); // beolvasunk egy sort, és a szóköz mentén széttöredezzük
+                    }
+
+                    int tableSize = int.Parse(datas[0]); // beolvassuk a játéktábla méretét
+                    int bordersNum = int.Parse(datas[1]); // beolvassuk az akadályok számát
+                    SnakeTable table = new SnakeTable(tableSize, bordersNum); // létrehozzuk a táblát
+
+                    //x/y koordinátái betöltése a GameFields listába
+                    int c = 2;
+                    for (int x = 0; x < int.Parse(datas[0]); x++)
+                    {
+                        //Oszlop feltölt
+                        for (int y = 0; y < int.Parse(datas[0]); y++)
+                        {
+                            if (c <= bordersNum && x == int.Parse(datas[c]) && y == int.Parse(datas[c + 1]))
+                            {
+                                SnakeField field = new SnakeField
+                                {
+                                    X = x,
+                                    Y = y,
+                                    Border = true
+
+                                };
+                                c += 2;
+                                table.FieldsCoordinate.Add(field);
+                            }
+                            else
+                            {
+                                SnakeField field = new SnakeField
+                                {
+                                    X = x,
+                                    Y = y,
+                                };
+                                table.FieldsCoordinate.Add(field);
+                            }
+                        }
+                    }
+
+                    return table;
+
+                }
+            }
+            catch
+            {
+
+                throw new SnakeDataException("Hibás elérési útvonal, a file nem létezik!");
+            }
+
+        }
+
+
+    }
+}
